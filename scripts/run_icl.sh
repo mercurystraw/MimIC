77eddd9c507c84377966ef4b899c9857cd1c1aed
Kami-chanw <865710157@qq.com>
2025-03-17 00:06:16 +0800

    minor fix

--- scripts/run_icl.sh
+++ scripts/run_icl.sh
@@ -7,10 +7,10 @@ cd ../src/
 # -s should be 0 for ICL.
 # to specify the number of shots, use --eval-args "eval.num_shot=32"
 python pipeline.py \
-    -r "$runname-idev-32shot" \
-    -d seed \
-    -m idefics-9b \
+    -r "$runname-llava-0shot" \
+    -d vqav2,ok_vqa,coco \
+    -m llava-interleave-7b \
     -e \
     -s 0 \
     --requires_memory 40000 \
-    --eval-args "eval.ckpt_epochs=null eval.batch_size=2 eval.num_shot=32"
+    --eval-args "eval.ckpt_epochs=null eval.batch_size=4 eval.num_shot=0 eval.query_set_size=1000"
